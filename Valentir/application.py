eaa3864f0145bc1f8ef8a42de223b52c4dc7e501
Cado Fett <brandon@krabach.net>
2025-04-30 15:20:43 -0400

    Waitress Deleted

--- Valentir/application.py
+++ Valentir/application.py
@@ -2,7 +2,6 @@
 #Used this video to help write this code:  https://www.youtube.com/watch?v=jQjjqEjZK58
 
 from flask import Flask, render_template, request, redirect, url_for
-from waitress import serve
 
 app = Flask(__name__)
 
@@ -16,5 +15,4 @@ def todo():
     return render_template('TODO.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True)
-    # serve (app, host="0.0.0.0", port=8000) // Doesn't work yet (Don't know why waitress isn't detected)
+    app.run(host="0.0.0.0", port=8000)
